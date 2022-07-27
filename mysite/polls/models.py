081c623d7875f684a6654af89f83406bd38cc578
kjh9571 <jhkim9571@gmail.com>
2022-07-27 11:31:01 +0900

    API 가지고 놀기

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,5 +1,7 @@
-from secrets import choice
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
@@ -7,7 +9,18 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
     
+    def __str__(self): # 객체의 표현을 대화식 프롬프트에서 편하게 보려는 이유 말고도, \
+        # Django가 자동으로 생성하는 관리 사이트에서도 객체의 표현이 사용되기 때문
+        return self.question_text
+    
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.choice_text
+    
