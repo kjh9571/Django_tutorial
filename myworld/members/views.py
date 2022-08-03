28086003ed9053765ce7ab04d419014a3ce99f66
kjh9571 <jhkim9571@gmail.com>
2022-08-03 17:27:31 +0900

    Code for Updating Records

--- myworld/members/views.py
+++ myworld/members/views.py
@@ -37,3 +37,12 @@ def update(request, id):
         'mymember' : mymember,
     }
     return HttpResponse(template.render(context, request))
+
+def updaterecord(request, id):
+    first = request.POST['first']
+    last = request.POST['last']
+    member = Members.objects.get(id=id)
+    member.firstname = first
+    member.lastname = last
+    member.save()
+    return HttpResponseRedirect(reverse('index'))
