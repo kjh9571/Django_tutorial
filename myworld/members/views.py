a6ee4971e14618d1a10970bedf47e247e767bd27
kjh9571 <jhkim9571@gmail.com>
2022-08-03 15:55:16 +0900

    Code for Adding Records

--- myworld/members/views.py
+++ myworld/members/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
+from django.urls import reverse
 from .models import Members
 
 # Create your views here.
@@ -17,3 +18,10 @@ def index(request):
 def add(request):
     template = loader.get_template('add.html')
     return HttpResponse(template.render({}, request))
+
+def addrecord(request):
+    x = request.POST['first']
+    y = request.POST['last']
+    member = Members(firstname=x, lastname=y)
+    member.save()
+    return HttpResponseRedirect(reverse('index'))
