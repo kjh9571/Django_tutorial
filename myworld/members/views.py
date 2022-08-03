a5fd3b6103c8cd70cccf78e5b6b9837890253b7e
kjh9571 <jhkim9571@gmail.com>
2022-08-03 14:59:41 +0900

    Change Settings

--- myworld/members/views.py
+++ myworld/members/views.py
@@ -5,7 +5,7 @@ from django.template import loader
 # Create your views here.
 
 def index(request):
-    template = loader.get_template('myfirst.html')
+    template = loader.get_template('mysecond.html')
     return HttpResponse(template.render())
 
 
