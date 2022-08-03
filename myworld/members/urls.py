9c4b886ddace1d328df8338d749ddff4edfe5236
kjh9571 <jhkim9571@gmail.com>
2022-08-03 17:22:13 +0900

    Updating Records: URLs

--- myworld/members/urls.py
+++ myworld/members/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('add/', views.add, name='add'),
     path('add/addrecord/', views.addrecord, name='addrecord'),
     path('delete/<int:id>', views.delete, name='delete'),
+    path('update/<int:id>', views.update, name='update'),
 ]
