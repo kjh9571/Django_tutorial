b71a6f715c9c73dfd9582b0b9e3525be2f4324e1
kjh9571 <jhkim9571@gmail.com>
2022-08-03 15:20:10 +0900

    Modify the View

--- myworld/members/views.py
+++ myworld/members/views.py
@@ -7,9 +7,11 @@ from .models import Members
 
 def index(request):
     mymembers = Members.objects.all().values()
-    output = ""
-    for x in mymembers:
-        output += x["firstname"]
-    return HttpResponse(output)
+    template = loader.get_template('index.html')
+    context = {
+        'mymembers' : mymembers
+    }
+ 
+    return HttpResponse(template.render(context, request))
 
 
